45d7def922439268c759e93980c0f878529bff3f
Sam Negus <sam.negus@outlook.com>
2023-12-08 23:23:05 -0600

    Fibonacci sequence challenges

---
+++ Multiples3sAnd5s/FibonacciEvenSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3sAnd5s
+{
+    internal class FibonacciEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 4000000;
+            Console.WriteLine("Find sum of all even numbers in the Fibonacci sequence that are under four million. (4,000,000)");
+            Console.WriteLine($"{ FindFibonacciSumUnder(maxNumber) }");
+        }
+
+        static int FindFibonacciSumUnder(int maxNumber)
+        {
+            int a = 0;
+            int b = 1;
+            int sum = 0;
+
+            while (b < maxNumber)
+            {
+                if (b % 2 == 0)
+                {
+                    sum += b;
+                }
+                
+                sum = a + b;
+                a = b;
+                b = sum;
+
+                
+                
+            };
+
+            return sum;
+
+
+
+        }
+
+    }
+}
---
+++ Multiples3sAnd5s/TenDigitFibonacci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3sAnd5s
+{
+    internal class TenDigitFibonacci
+    {
+        static void Main()
+        {
+            int digits = 10;
+
+            Console.WriteLine($"Find the index of the first Fibonacci number with {digits} digits...");
+
+            int result = FindFibonacciWithDigits(digits, out int index);
+
+            Console.WriteLine($"The first Fibonacci number with {digits} digits is {result} at index {index}");
+        }
+
+        static int FindFibonacciWithDigits(int targetDigits, out int index)
+        {
+            int a = 0;
+            int b = 1;
+            int sum;
+            int currentIndex = 1;
+
+            while (CountDigits(b) < targetDigits)
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+                currentIndex++;
+            }
+
+            index = currentIndex;
+            return b;
+        }
+
+        static int CountDigits(int number)
+        {
+            int count = 0;
+            while (number != 0)
+            {
+                number /= 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}
